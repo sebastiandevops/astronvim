1bd433fa6e44e5558ae039d89779ac91f991fe19
Sebastián Valencia <sebasvalencia726@gmail.com>
2024-05-02 20:30:49 -0500

    feat: file_format added to heirline using heirline-components.nvim plugin.

--- lua/plugins/overridden.lua
+++ lua/plugins/overridden.lua
@@ -9,5 +9,31 @@ return {
         opts.background_colour = "#282828"
       end
     end,
-  }
+  },
+  {
+    "rebelot/heirline.nvim",
+    dependencies = {
+      {
+        "Zeioth/heirline-components.nvim",
+      }
+    },
+    opts = function(_, opts)
+      -- list like portions of a table cannot be merged naturally and require the user to merge it manually
+      -- check to make sure the key exists
+      if not opts.statusline then
+        opts.statusline = {}
+      end
+      local function insert_at(tbl, index, value)
+          table.insert(tbl, index, value)
+      end
+      local heirline_components = require "heirline-components.all"
+      local fileformat = function()
+        local fmt = heirline_components.component.file_encoding({ file_encoding = false, file_format = { padding = { left = 2, right = 0 } }})
+        return fmt
+      end
+      insert_at(opts.statusline, 10, {
+        fileformat(),
+      })
+    end,
+  },
 }
