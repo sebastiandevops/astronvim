beed86a449304196569f1b2761978214e8ac2cc1
Sebastián Valencia <sebasvalencia726@gmail.com>
2024-05-12 17:40:10 -0500

    update: added space and eol chars to listchars.

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -5,7 +5,14 @@ local set = vim.opt  -- Reference to Neovim options table
 set.list = true  -- Enable display of special characters
 
 -- Configure display of special characters:
-set.listchars = { tab = '»·', trail = '·', extends = '→', precedes = '←' }
+set.listchars = {
+  space = '⋅',
+  eol = '¶',
+  tab = '⇢ ',
+  trail = '·',
+  extends = '→',
+  precedes = '←'
+}
 
 set.conceallevel = 2                        -- Enable formatting concealment features
 set.fillchars = { eob = " ", vert = " " }   -- Set the fill characters for end of buffer and vertical split to be a space
